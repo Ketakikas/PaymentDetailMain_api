91b91b0e71f30d72e3659dbbba40356be4b99ce4
Ketaki Kashyap <Ketakikashyap1991@gmail.com>
2022-03-10 14:43:04 +0530

    initial

---
+++ Payment_DetailsAPI/Controllers/PaymentDetailsController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PaymentAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentDetailsController : ControllerBase
+    {
+        private readonly PaymentDetailContext _context;
+        public PaymentDetailsController(PaymentDetailContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<PaymentDetails>> GetPaymentDetails()
+        {
+            return await _context.paymentDetails.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PaymentDetails>> GetPaymentDetailById(int id)
+        {
+            var details =await _context.paymentDetails.FindAsync(id);
+            if(details==null)
+            {
+                return NotFound();
+            }
+            return details;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<PaymentDetails>> PostPaymentDetails(PaymentDetails details)
+        {
+            _context.paymentDetails.Add(details);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetPaymentDetailById", new { id = details.PaymentDetailId }, details);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<PaymentDetails>> PutPaymentDetails(int id,PaymentDetails details)
+        {
+            if(id!=details.PaymentDetailId)
+            {
+                return BadRequest();
+            }
+            _context.Entry(details).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch(DbUpdateConcurrencyException)
+            {
+                if(!PaymentDetailExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<PaymentDetails>> DeletePaymentDetails(int id)
+        {
+            var detail = _context.paymentDetails.FirstOrDefault(x => x.PaymentDetailId == id);
+            if(detail==null)
+            {
+                return NotFound();
+            }
+            _context.paymentDetails.Remove(detail);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PaymentDetailExists(int id)
+        {
+            return _context.paymentDetails.Any(x => x.PaymentDetailId == id);
+        }
+    }
+}
---
+++ Payment_DetailsAPI/Models/PrepDB.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using PaymentAPI.Models;
+
+namespace Payment_DetailsAPI.Models
+{
+    public class PrespDB
+    {
+        // remove connection string and migrations folder
+        public static void PresPopulation(IApplicationBuilder app)
+        {
+            using(var serviceScope=app.ApplicationServices.CreateScope())
+            {
+                SendData(serviceScope.ServiceProvider.GetService<PaymentDetailContext>());
+            }
+        }
+        private static void SendData(PaymentDetailContext context)
+        {
+            Console.WriteLine("Applying Migration...");
+            context.Database.Migrate();//to create migrations
+            if(context.paymentDetails.Any())
+            {
+                context.paymentDetails.AddRange(new PaymentDetails() { 
+                    CardNumber="1234567890123456",
+                    CardOwnerName="John Doe",
+                    SecurityCode="123",
+                    ExpirationDate="02/24"
+                });
+
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("Already have data...no seeding");
+            }
+        }
+    }
+}
